7be27545e4a24350bc35aeac10506c095e987289
Mark Kuchel <mark@kuchel.net>
2021-12-16 11:35:56 +1100

    update device_ to extra_ for attributes

--- custom_components/vicemergency_feed/geo_location.py
+++ custom_components/vicemergency_feed/geo_location.py
@@ -340,7 +340,7 @@ class VICEmergencyLocationEvent(GeolocationEvent):
         return LENGTH_KILOMETERS
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the device state attributes."""
         attributes = {}
         for key, value in (
